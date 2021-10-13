305a0fbc4ef24ef90c66b0bbccef961cd36d0686
C0ss4ck <c0ss4ck9989@gmail.com>
2021-10-13 21:34:19 +0800

    more args

--- mz_solve.py
+++ mz_solve.py
@@ -8,22 +8,43 @@ output = open("D:\\Project\\PythonProject\\datacon2021\\res.txt", "a+")
 register = idaapi.ph_get_regnames()
 
 if "R0" in register:
-    arch = ["R0", "R1", "R2"]       # ARM
-    inst = ["MOV","LDR"]
+    arch = "arm"
+    pvar = ["R0", "R1", "R2", "R3", "[SP]"]       # ARM
+    inst = {
+        "1": ["MOV", "LDR"],
+        "2": ["STR"]
+    }
 elif "$a0" in register:
-    arch = ["$a0", "$a1", "$a2"]    #MIPS
-    inst = ["move","addiu"]
+    arch = "mips"
+    pvar = ["$a0", "$a1", "$a2", "$a3", "0x10($sp)"]    #MIPS
+    inst = {
+        "1": ["move", "addiu", "lw"],
+        "2": ["sw"]
+    }
 elif "r15" in register:
-    arch = ["rdi", "rsi", "rdx"]    #x86
-    inst = ["mov", "add"]
+    arch = "amd64"
+    pvar = ["rdi", "rsi", "rdx", "rcx", "r8", "r9"]    #x86
+    inst = ["mov", "lea"]
 else:
     print("NOT SUPPORT ARCH")
     exit(0)
 
 
-def check(current_rip,start_ea,target_register, check_const):
+def check(current_rip,start_ea,target_register_idx, check_const):
+    local_vars = {
+        "final_var": '',
+        "current_var": pvar[target_register_idx]
+    }
+    if target_register_idx > 3 and arch in ["arm", "mips"]:
+        while current_rip != start_ea:
+            if idc.print_insn_mnem(current_rip) in inst["2"] and idc.op_hex(current_rip, 1) and idc.print_operand(current_rip, 1) == local_vars["current_var"]:
+                local_vars["final_var"] = idc.print_operand(current_rip, 0)
+                break
+            current_rip = idc.prev_head(current_rip)
+    else:
+        local_vars["final_var"] = local_vars["current_var"]
     while current_rip != start_ea:
-        if idc.print_insn_mnem(current_rip) in inst and idc.print_operand(current_rip,0) == target_register:
+        if idc.print_insn_mnem(current_rip) in inst["1"] and idc.print_operand(current_rip, 0) == local_vars["final_var"]:
             asm = idc.GetDisasm(current_rip)
             if check_const is True:
                 if ("\"" in asm) and (";" in asm or "#" in asm):
@@ -39,99 +60,140 @@ def check(current_rip,start_ea,target_register, check_const):
     return 0
 
 
+def check_with_va_args(current_rip, start_ea):
+    while current_rip != start_ea:
+        if idc.print_insn_mnem(current_rip) in inst["1"] and idc.print_operand(current_rip, 0) == pvar[0]:
+            asm = idc.GetDisasm(current_rip)
+            if ("\"" in asm) and (";" in asm or "#" in asm):
+                print(asm)
+                break
+            else:
+                return 0
+        current_rip = idc.prev_head(current_rip)
+
+
 def check_vuln(ea,check_point):
     code_xrefs = idautils.CodeRefsTo(ea, False)
     for code_xref in code_xrefs:
         xref_function_address = idc.get_func_attr(code_xref, idc.FUNCATTR_START)
         if xref_function_address & 0xffffffff != 0xffffffff:
-            result = check(code_xref,xref_function_address,arch[check_point['args']-1], check_point["const"])
-            print(f'{idaapi.get_root_filename()},{idc.get_func_name(ea)},{hex(code_xref-idaapi.get_imagebase())},{result}')
-            output.write(f'{idaapi.get_root_filename()},{idc.get_func_name(ea)},{hex(code_xref-idaapi.get_imagebase())},{result}\n')
+            if "va_args" not in check_point.keys() and "const" in check_point.keys():
+                result = check(code_xref,xref_function_address,check_point['args']-1, check_point["const"])
+                print(f'{idaapi.get_root_filename()},{idc.get_func_name(ea)},{hex(code_xref-idaapi.get_imagebase())},{result}')
+                output.write(f'{idaapi.get_root_filename()},{idc.get_func_name(ea)},{hex(code_xref-idaapi.get_imagebase())},{result}\n')
+            elif "va_args" in check_point.keys() and "const" in check_point.keys():
+                check_with_va_args(code_xref,xref_function_address)
+            else:
+                print(f'{idaapi.get_root_filename()},{idc.get_func_name(ea)},{hex(code_xref - idaapi.get_imagebase())},0')
+                output.write(f'{idaapi.get_root_filename()},{idc.get_func_name(ea)},{hex(code_xref - idaapi.get_imagebase())},0\n')
         else:
             pass
 
 
 if __name__ == '__main__':
     check_list = [
-      {
-        "function": "system",
-        "args": 1,
-          "const": True
-      },
-      {
-        "function": "popen",
-        "args": 1,
-          "const": True
-      },
-      {
-        "function": "sprintf",
-        "args": 2,
-          "const": True
-      },
-      {
-        "function": "snprintf",
-        "args": 3,
-          "const": True
-      },
-      {
-        "function": "_system",
-        "args": 1,
-          "const": True
-      },
-      {
-        "function": "_popen",
-        "args": 1,
-          "const": True
-      },
-      {
-        "function": "_sprintf",
-        "args": 2,
-          "const": True
-      },
-      {
-        "function": "_snprintf",
-        "args": 3,
-          "const": True
-      },
-      {
-        "function": "AES_set_encrypt_key",
-          "args": 1,
-          "const": False
-      },
-      {
-        "function": "AES_set_decrypt_key",
-          "args": 1,
-          "const": False
-      },
-      {
-        "function": "_AES_set_encrypt_key",
-          "args": 1,
-          "const": False
-      },
-      {
-        "function": "_AES_set_decrypt_key",
-          "args": 1,
-          "const": False
-      },
+        {
+            "function": "system",
+            "args": 1,
+            "const": True
+        },
+        {
+            "function": "popen",
+            "args": 1,
+            "const": True
+        },
+        {
+            "function": "sprintf",
+            "args": 2,
+            "const": True
+        },
+        {
+            "function": "snprintf",
+            "args": 3,
+            "const": True
+        },
+        {
+            "function": "_system",
+            "args": 1,
+            "const": True
+        },
+        {
+            "function": "_popen",
+            "args": 1,
+            "const": True
+        },
+        {
+            "function": "_sprintf",
+            "args": 2,
+            "const": True
+        },
+        {
+            "function": "_snprintf",
+            "args": 3,
+            "const": True
+        },
+        {
+            "function": "AES_set_encrypt_key",
+            "args": 1,
+            "const": False
+        },
+        {
+            "function": "AES_set_decrypt_key",
+            "args": 1,
+            "const": False
+        },
+        {
+            "function": "_AES_set_encrypt_key",
+            "args": 1,
+            "const": False
+        },
+        {
+            "function": "_AES_set_decrypt_key",
+            "args": 1,
+            "const": False
+        },
         {
             "function": "EVP_DecryptInit_ex",
-            "args": 3,
+            "args": 4,
             "const": False
         },
         {
             "function": "EVP_EncryptInit_ex",
-            "args": 3,
+            "args": 4,
             "const": False
         },
         {
             "function": "_EVP_DecryptInit_ex",
-            "args": 3,
+            "args": 4,
             "const": False
         },
         {
             "function": "_EVP_EncryptInit_ex",
-            "args": 3,
+            "args": 4,
             "const": False
+        },
+        {
+            "function": "DES_set_key_checked",
+        },
+        {
+            "function": "doSystem",
+            "va_args": True,
+            "const": True
+        },
+        {
+            "function": "doSystembk",
+            "va_args": True,
+            "const": True
+        },
+        {
+            "function": "doSystemCmd",
+            "va_args": True,
+            "const": True
+        },
+        {
+            "function": "COMMAND",
+            "va_args": True,
+            "const": True
         }
     ]
 
